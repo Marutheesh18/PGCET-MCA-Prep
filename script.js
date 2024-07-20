3566b35e5859fda6380833272d74a3963da5f0f8
Marutheesh18 <desidasher1718@gmail.com>
2024-07-20 13:48:43 +0530

    Write Ratings And Review

--- script.js
+++ script.js
@@ -30,7 +30,33 @@ const swiper = new Swiper('.slider-wrapper', {
         slidesPerView: 2
       },
       1024: {
-        slidesPerView: 3
+        slidesPerView: 4
       }
     }
   });
+
+  // rating and review
+
+  const allStar = document.querySelectorAll('.rating .star')
+const ratingValue = document.querySelector('.rating input')
+
+allStar.forEach((item, idx)=> {
+	item.addEventListener('click', function () {
+		let click = 0
+		ratingValue.value = idx + 1
+
+		allStar.forEach(i=> {
+			i.classList.replace('bxs-star', 'bx-star')
+			i.classList.remove('active')
+		})
+		for(let i=0; i<allStar.length; i++) {
+			if(i <= idx) {
+				allStar[i].classList.replace('bx-star', 'bxs-star')
+				allStar[i].classList.add('active')
+			} else {
+				allStar[i].style.setProperty('--i', click)
+				click++
+			}
+		}
+	})
+})
